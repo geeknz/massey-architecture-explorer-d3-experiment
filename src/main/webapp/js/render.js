4f375973497390be359132f8390549c3a2b4693d
Colin Campbell <coder@colin.geek.nz>
2013-10-16 20:17:54 +1300

    Added jquery and jquery url plugin

--- src/main/webapp/js/render.js
+++ src/main/webapp/js/render.js
@@ -1,21 +1,40 @@
 requirejs.config({
+
+	paths : {
+		'jquery' : '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
+		'jquery.url' : '//cdn.jsdelivr.net/jquery.url.parser/2.2.1/purl',
+		'd3' : 'lib/d3.v3.min',
+		'hashtable' : 'lib/hashtable',
+		'font' : 'lib/Font'
+	},
+
 	shim: {
 
-		'lib/d3.v3.min' : {
+		'jquery' : {
+			exports : 'jQuery'
+		},
+
+		'jquery.url' : {
+			deps: ['jquery']
+		},
+
+		'd3' : {
 			exports: 'd3'
 		},
 
-		'lib/hashtable' : {
+		'hashtable' : {
 			exports: 'Hashtable'
 		},
 
-		'lib/Font' : {
+		'font' : {
 			exports: 'Font'
 		}
 	}
 });
 
-require( [ 'lib/Font', 'lib/hashtable', 'lib/d3.v3.min' ], function( Font, HashTable, d3 ) {
+require( [ 'font', 'hashtable', 'd3', 'jquery', 'jquery.url' ], function( Font, HashTable, d3, $ ) {
+
+	var data = $.url().param( 'data' );
 
 	var font = new Font();
 
